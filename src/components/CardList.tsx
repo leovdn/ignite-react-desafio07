a4fd97ec806219fb2d58c3e25e67afa5e8d44965
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-01 20:51:20 -0300

    feat: image modal

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,31 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [image, setImage] = useState('');
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(image) {
+    setImage(image);
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid onClick={onOpen}>
+        {cards.map(card => (
+          <Card
+            data={card}
+            key={card.id}
+            viewImage={() => handleViewImage(card.url)}
+          />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={image} />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,26 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered autoFocus={false}>
+      <ModalOverlay />
+      <ModalContent
+        bg="pGray.800"
+        margin={4}
+        w="100%"
+        maxWidth={900}
+        maxHeight={640}
+      >
+        <ModalBody p="0">
+          <Image src={imgUrl} w="100%" maxWidth={900} maxHeight={600} />
+        </ModalBody>
+
+        <ModalFooter display="flex" justifyContent="flex-start" p="8px 10px">
+          <Link href={imgUrl} target="_blank" fontSize="14">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
